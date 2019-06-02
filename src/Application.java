3ebbd4cd3df7dc0dd9443b26ab216177c3058a6e
Eugene <antipov.jn@gmail.com>
2019-06-02 15:38:08 +0300

    Home Work 3.1 Code Style fixed

--- src/Application.java
+++ src/Application.java
@@ -2,15 +2,17 @@ import java.math.BigInteger;
 
 public class Application {
     public static void main(String[] args) {
-        int[] x = mergeArrays(new int[] {10, 11, 41, 50, 65, 86, 98, 101, 190, 1100, 1200, 3000, 5000},new int[] {1, 3, 5, 7, 8,10,10});
-        for (int i:x) {
+
+        int[] x = mergeArrays(new int[]{10, 11, 41, 50, 65, 86, 98, 101, 190, 1100, 1200, 3000, 5000}, new int[]{1, 3, 5, 7, 8, 10, 10});
+        for (int i : x) {
             System.out.print(i + ",");
         }
-        String [] roles = {
+
+        String[] roles = {
                 "Городничий", "Аммос Федорович",
                 "Артемий Филиппович",
                 "Лука Лукич"};
-        String [] textLines = {
+        String[] textLines = {
                 "Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
                 "Аммос Федорович: Как ревизор?",
                 "Артемий Филиппович: Как ревизор?",
@@ -19,26 +21,33 @@ public class Application {
                 "Артемий Филиппович: Вот не было заботы, так подай!",
                 "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
         System.out.println(printTextPerRole(roles, textLines));
-        robotMove(new Robot(),-2,-2);
+
+        robotMove(new Robot(), -2, -2);
+
         System.out.println(factorial(13));
+
         System.out.println(isPalindrome("Madam, I'm Adam!") ? "t" : "f");
+
         byte[] example = {72, 101, 108, 108, 111, 33};
         AsciiCharSequence answer = new AsciiCharSequence(example);
         System.out.println("Последовательность - " + answer.toString()); //Hello!
         System.out.println("Размер её - " + answer.length()); //6
         System.out.println("Символ под № 1 - " + answer.charAt(1)); //e
-        System.out.println("Подпоследовательность - " + answer.subSequence(1, 5)); //ello --> проверка на нарушение инкапсуляции private поля
+        System.out.println("Подпоследовательность - " + answer.subSequence(1, 5)); //ello
+        System.out.println("Не правильные индексы -" + answer.subSequence(4, 1));
         System.out.println(answer.toString()); //Hello!
-        example[0] = 74;
+        example[0] = 74; //check for encapsulation violation of field
         System.out.println(answer.toString()); //Hello!
-        ComplexNumber firstNumber = new ComplexNumber(123,321);
+
+        ComplexNumber firstNumber = new ComplexNumber(123, 321);
         ComplexNumber secondNumber = new ComplexNumber(123, 321);
         ComplexNumber thirdNumber = new ComplexNumber(213, 321);
         System.out.println(firstNumber.equals(secondNumber) + " " + firstNumber.toString() + " equals " + secondNumber.toString());
         System.out.println(firstNumber.equals(thirdNumber) + " " + firstNumber.toString() + " !equals " + thirdNumber.toString());
+
         String text = " spam bad";
         String[] spam = {"spam", "bad"};
-        System.out.println(checkLabels(new TextAnalyzer[]{new NegativeTextAnalyzer(), new SpamAnalyzer(spam),new TooLongTextAnalyzer(30)}, text));
+        System.out.println(checkLabels(new TextAnalyzer[]{new NegativeTextAnalyzer(), new SpamAnalyzer(spam), new TooLongTextAnalyzer(30)}, text));
 
     }
 
@@ -50,20 +59,20 @@ public class Application {
     }
 
     private static BigInteger factorial(int value) {
-        BigInteger fact = BigInteger.valueOf(1);
+        BigInteger factorial = BigInteger.valueOf(1);
         for (int i = 1; i <= value; i++) {
-            fact = fact.multiply(BigInteger.valueOf(i));
+            factorial = factorial.multiply(BigInteger.valueOf(i));
         }
-        return fact;
+        return factorial;
     }
 
-    private static void robotMove (Robot robot, int toX, int toY) {
+    private static void robotMove(Robot robot, int toX, int toY) {
         if (robot.getX() < toX) {
             while (robot.getDirection() != Direction.RIGHT) {
                 if (robot.getDirection().equals(Direction.DOWN)) {
                     robot.turnLeft();
                 } else {
-                  robot.turnRight();
+                    robot.turnRight();
                 }
             }
             while (robot.getX() < toX) {
@@ -86,7 +95,7 @@ public class Application {
                 if (robot.getDirection().equals(Direction.RIGHT)) {
                     robot.turnRight();
                 } else {
-                robot.turnLeft();
+                    robot.turnLeft();
                 }
             }
             while (robot.getY() > toY) {
@@ -105,14 +114,12 @@ public class Application {
                 robot.stepForward();
             }
         }
-
     }
-
 
 
     private static String printTextPerRole(String[] roles, String[] textLines) {
         StringBuilder sb = new StringBuilder();
-        for (String s:roles) {
+        for (String s : roles) {
             sb.append(s).append(":\n");
             for (int x = 0; x < textLines.length; x++) {
                 if (textLines[x].startsWith(s + ":")) {
@@ -123,7 +130,6 @@ public class Application {
         }
         return sb.toString();
     }
-
 
 
     private static int[] mergeArrays(int[] a1, int[] a2) {
@@ -143,16 +149,15 @@ public class Application {
                 n++;
             }
         }
-
         return mergeArray;
     }
 
     public static Label checkLabels(TextAnalyzer[] analyzers, String text) {
-            for (TextAnalyzer analyzer: analyzers) {
-                if (analyzer.processText(text) != Label.OK) {
-                    return analyzer.processText(text);
-                }
+        for (TextAnalyzer analyzer : analyzers) {
+            if (analyzer.processText(text) != Label.OK) {
+                return analyzer.processText(text);
             }
+        }
         return Label.OK;
     }
 }
